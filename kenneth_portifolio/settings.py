7a90aa6c1147778eda9c3e3526ed477bcc5f6da4
Martin Rhine <tusingwiremartinrhinetreviz@gmail.com>
2024-09-17 14:29:18 +0300

    added Trusted CORS

--- kenneth_portifolio/settings.py
+++ kenneth_portifolio/settings.py
@@ -62,7 +62,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CSRF_TRUSTED_ORIGINS = ["https://kenneth-portifolio-backend.up.railway.app/", "http://kenneth-portifolio-backend.up.railway.app"]
+CSRF_TRUSTED_ORIGINS = ["https://kenneth-portifolio-backend.up.railway.app", "http://kenneth-portifolio-backend.up.railway.app"]
 
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5500",
